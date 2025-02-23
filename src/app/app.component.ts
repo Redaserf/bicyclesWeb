aeee58048b90a325ef44c69d4ccb47d2f34a68e1
Redaserf <hugormzvillarreal@gmail.com>
2025-02-23 12:52:55 -0600

    Directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogoComponent } from "./pages/all/logo/logo.component";
+import { NgFor } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LogoComponent],
+  imports: [RouterOutlet, LogoComponent, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'bicyclesWeb';
+  
+  opcionesSeguimiento = [
+    {nombre: "Iniciar Sesión", ruta: "#"},
+    {nombre: "Informacion", ruta: "#"},
+    {nombre: "Descargar-App", ruta: "#"},
+    {nombre: "Contactanos", ruta: "#"},
+  ];
+
 }
