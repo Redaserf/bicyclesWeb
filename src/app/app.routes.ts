f37e08867c6da359db2695e9212f2b6e1bedc338
Anggel87 <angelheriberto87@gmail.com>
2025-02-25 19:05:28 -0600

    [ADD] - Login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { GuestLayoutComponent } from './pages/guest/layout/guest-layout/guest-layout.component';
+import { LoginComponent } from './pages/guest/login/login.component';
 
 export const routes: Routes = [
 
     {path: '', component: GuestLayoutComponent, pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
 
 ];
