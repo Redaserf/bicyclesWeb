0564501101babd09280744388abb4f20a1ba65dd
AldebaranAlba <aldebaran.alba0987@gmail.com>
2025-02-25 21:48:11 -0600

    ruta register con code
    ruta register con ruta code de anyelo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { Routes } from '@angular/router';
 import { GuestLayoutComponent } from './pages/guest/layout/guest-layout/guest-layout.component';
 import { LoginComponent } from './pages/guest/login/login.component';
 import { CodeVerificationComponent } from './pages/guest/code-verification/code-verification.component';
+import {RegisterComponent} from './pages/guest/register/register.component';
 
 export const routes: Routes = [
 
     {path: '', component: GuestLayoutComponent, pathMatch: 'full'},
     {path: 'code', component: CodeVerificationComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+
 
 ];
