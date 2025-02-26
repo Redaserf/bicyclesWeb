c12fc4ce56368c428514e765079b790fca904b37
AldebaranAlba <aldebaran.alba0987@gmail.com>
2025-02-25 21:42:04 -0600

    UPD - Register con diseño
    Register con diseño

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { GuestLayoutComponent } from './pages/guest/layout/guest-layout/guest-layout.component';
 import { LoginComponent } from './pages/guest/login/login.component';
+import {RegisterComponent} from './pages/guest/register/register.component';
 
 export const routes: Routes = [
 
     {path: '', component: GuestLayoutComponent, pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
 
 ];
--- src/app/pages/guest/register/register.component.ts
+++ src/app/pages/guest/register/register.component.ts
@@ -1,25 +1,48 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {NgIf} from '@angular/common';
+import * as AOS from 'aos';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule,NgIf],
+  imports: [FormsModule,FontAwesomeModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewInit{
 
   userObject: any = {
-    firstName: '',
-    lastName: '',
-    weight: '',
+    nombre: '',
+    apellido: '',
+    peso: '',
     email: '',
     password: '',
   }
 
+  confirm_password: string = '';
+
+  ngAfterViewInit(): void {
+    AOS.init({
+      offset: 100,
+      delay: 0,
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false
+    });
+
+  }
+
   save(){
-    const form_value = this.userObject;
+
+
+
+    if(this.confirm_password == this.userObject.password){
+
+      const form_value = this.userObject;
+      console.log(form_value);
+
+    }
   }
 
 }
