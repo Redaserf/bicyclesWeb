f9ee6ffcca92cc13edbf3ee5836b1f787edae561
Redaserf <hugormzvillarreal@gmail.com>
2025-03-28 19:54:00 -0600

    [ADD]- implementacion de la estadistica en usuario

--- src/app/components/user/user-estadisticas/user-estadisticas.component.ts
+++ src/app/components/user/user-estadisticas/user-estadisticas.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import AOS from 'aos';
 import Chart from 'chart.js/auto';
+import { ApiService } from '../../../services/api-service.service';
+import { CargaService } from '../../../services/carga.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-estadisticas',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './user-estadisticas.component.html',
   styleUrl: './user-estadisticas.component.css'
 })
 export class UserEstadisticasComponent {
+
+  cargando: boolean = false;
+  constructor(private api: ApiService, private cargaService: CargaService) { }
+
   ngOnInit(): void {
+
+    this.cargaService.show()
+    this.cargaService.cargando$.subscribe((cargando: boolean) => {
+      this.cargando = cargando;
+    });
+
         AOS.init({
           offset: 100,
           delay: 0,
@@ -17,20 +30,69 @@ export class UserEstadisticasComponent {
           easing: 'ease-in-out',
           once: false
         });
-      }
-      selectedFilter: string = 'week';
-    selectedTab: string = 'calories';
+
+        this.crearGrafica();
+  }
+    selectedFilter: string = 'week';
+    selectedTab: string = 'calorias';
     chart!: Chart;
+    generales!: any;
+    chartData!: any;
 
-    data = {
-      distance: { total: 50, best: 12, average: 8 },
-      calories: { total: 3556, best: 615, average: 508 },
-      duration: { total: 180, best: 50, average: 30 }
-    };
 
-    chartData = {
-      week: [495, 375, 488, 487, 509],
-      month: [1500, 1200, 1800, 1750, 1600],
-      year: [20000, 18000, 22000, 19500, 21000]
-    };
+
+    crearGrafica(tab: string = 'calorias') {
+      this.selectedTab = tab;
+      const ctx = document.getElementById('miGrafico') as HTMLCanvasElement;
+    
+      // Si ya existe una gráfica previa, la destruimos para no superponer
+      if (this.chart) {
+        this.chart.data.datasets[0].data = this.chartData[tab];
+        this.chart.update();
+      }else{
+          this.api.get('semana/estadisticas').then((response: any) => {
+            if (response.status === 200) {
+              this.generales = response.data.generales;
+                
+              this.chartData = response.data.data;
+    
+              this.chart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                  labels: response.data.labels,
+                  datasets: [
+                    {
+                      label: `${tab}`,
+                      data: response.data.data[tab],
+                      borderWidth: 1
+                    }
+                  ]
+                },
+                options: {
+                  scales: {
+                    y: {
+                      beginAtZero: true
+                    }
+                  },
+                  responsive: true,
+                  maintainAspectRatio: false
+                }
+              });
+
+              this.cargaService.hide()
+            }
+          }).catch((error: any) => {
+            console.error('Error al obtener los datos:', error);
+            this.cargaService.hide()
+          });
+      }
+
+    }
+    
+  cambiarTab(tab: string) {
+    this.selectedTab = tab;
+    this.crearGrafica();
+  }
+
+
 }
